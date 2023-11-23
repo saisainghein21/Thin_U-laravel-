008d6df841fe6e42d9f327bca20e99a43926cfe4
saisainghein21 <saisainghein2122003@gmail.com>
2023-11-23 22:51:35 +0630

    upload laravel

---
+++ database/migrations/2023_10_10_164946_create_posts_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table -> integer('user_id');
+            $table -> longText('title')->nullable();
+            $table -> longText('body');
+            $table -> string('image') -> nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
